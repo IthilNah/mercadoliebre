3b41b4cd8d104287d74ab969958ea7e8c3f961b1
IthilNah <alankasis@hotmail.com>
2022-04-19 03:19:08 +0200

    Falta register form y estamo

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ app.listen(3001, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/views/index.html'));
 })
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/login.html'));
+})
